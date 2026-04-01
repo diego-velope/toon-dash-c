6abee965f4e716371157ee4e8e856dbda1179c69
Diego Fuenmayor <diego@velope.tv>
2026-04-01 18:23:12 -0400

    fix

--- web/toon_dash.js
+++ web/toon_dash.js
@@ -70,7 +70,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmp5fc440ye.js
+// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpv10il5o1.js
 
 if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
 Module['expectedDataFileDownloads']++;
@@ -363,21 +363,21 @@ Module['expectedDataFileDownloads']++;
   });
 })();
 
-// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmp5fc440ye.js
-// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpggmzqdih.js
+// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpv10il5o1.js
+// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpcxichnin.js
 
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER || typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD || typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET) Module['preRun'] = [];
 var necessaryPreJSTasks = Module['preRun'].slice();
-// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpggmzqdih.js
-// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmp7p45axqq.js
+// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpcxichnin.js
+// include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpoqpl4zph.js
 
 if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
 necessaryPreJSTasks.forEach(task => {
   if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
 });
-// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmp7p45axqq.js
+// end include: /var/folders/_z/ybfm1r1s0y172xcf4lr_jttm0000gn/T/tmpoqpl4zph.js
 
 var arguments_ = [];
 var thisProgram = './this.program';
